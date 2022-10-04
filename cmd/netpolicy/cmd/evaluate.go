96d148e21e6c7cbf82174425430350d07328fa1a
Etai Lev Ran <etail@il.ibm.com>
2022-10-04 11:46:30 +0300

    Integrate with a running cluster
    * create a client set
    * load objects from cluster
    * evaluate connection
    
    Added comments for future cleanup tasks.
    
    Signed-off-by: Etai Lev Ran <etail@il.ibm.com>

--- cmd/netpolicy/cmd/evaluate.go
+++ cmd/netpolicy/cmd/evaluate.go
@@ -14,12 +14,17 @@
 package cmd
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
+	corev1 "k8s.io/api/core/v1"
+	netv1 "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 
 	"github.com/np-guard/netpol-analyzer/pkg/netpol/eval"
@@ -32,12 +37,12 @@ import (
 var (
 	help bool
 	// evaluated connection information
-	protocol      = "tcp"
-	sourcePod     = types.NamespacedName{Namespace: "default"}
-	targetPod     = types.NamespacedName{Namespace: "default"}
-	srcExternalIP string
-	dstExternalIP string
-	port          string
+	protocol       = "tcp"
+	sourcePod      = types.NamespacedName{Namespace: "default"}
+	destinationPod = types.NamespacedName{Namespace: "default"}
+	srcExternalIP  string
+	dstExternalIP  string
+	port           string
 	// cluster access information
 	kubecontext string
 	kubeconfig  string
@@ -47,63 +52,123 @@ var (
 var evaluateCmd = &cobra.Command{
 	Use:     "evaluate",
 	Short:   "Evaluate if a specific connection allowed",
-	Aliases: []string{"eval", "check", "allow"},
+	Aliases: []string{"eval", "check", "allow"}, // @etail - close on fewer, consider changing command name?
 
-	// @etail: can this check be done in an Args function?
+	// @etail: can this check be done in an Args function (e.g., incl. built-in's such as MinArgs(3))?
 	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-		if targetPod.Name == "" && dstExternalIP == "" {
-			return errors.New("no destination defined, target-pod and namespace or external IP required")
-		} else if targetPod.Name != "" && dstExternalIP != "" {
-			return errors.New("only one of target-pod and namespace or external IP can be defined, not both")
-		}
 		if sourcePod.Name == "" && srcExternalIP == "" {
-			return errors.New("no source defined, source-pod and namespace or external IP required")
+			return errors.New("no source defined, source pod and namespace or external IP required")
 		} else if sourcePod.Name != "" && srcExternalIP != "" {
-			return errors.New("only one of source-pod and namespace or external IP can be defined, not both")
+			return errors.New("only one of source pod and namespace or external IP can be defined, not both")
 		}
+
+		if destinationPod.Name == "" && dstExternalIP == "" {
+			return errors.New("no destination defined, destination pod and namespace or external IP required")
+		} else if destinationPod.Name != "" && dstExternalIP != "" {
+			return errors.New("only one of destination pod and namespace or external IP can be defined, not both")
+		}
+
 		if srcExternalIP != "" && dstExternalIP == "" {
-			return errors.New("only one of srcExternalIP or dstExternalIP can be defined, not both")
+			return errors.New("only one of source or destination can be defined as external IP, not both")
 		}
+
 		if port == "" {
-			return errors.New("target port name or value is required")
+			return errors.New("destination port name or value is required")
 		}
+
 		return nil
 	},
 
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		destination := dstExternalIP
 		if destination == "" {
-			destination = targetPod.String()
+			destination = destinationPod.String()
 		}
+
 		source := srcExternalIP
 		if source == "" {
 			source = sourcePod.String()
 		}
-		fmt.Printf("dest: %v", destination)
-		fmt.Printf("source: %v", source)
 
-		_, err := eval.CheckIfAllowed(source, destination, protocol, port)
-		if err != nil {
-			fmt.Printf("error CheckIfAllowed: %v", err)
+		// @etail: add explicit logs to indicate progress (loading config, listing namespaces, ...)
+
+		// create a k8s client with the correct config and context
+		loadingRules := &clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeconfig}
+		overrides := &clientcmd.ConfigOverrides{}
+		if kubecontext != "" {
+			overrides.CurrentContext = kubecontext
 		}
+
+		k8sconf, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, overrides).ClientConfig()
+		if err != nil {
+			return err
+		}
+		clientset, err := kubernetes.NewForConfig(k8sconf)
+		if err != nil {
+			return err
+		}
+
+		// @etail: use errors.Wrap for clearer error return?
+
+		namespaces := []*corev1.Namespace{}
+		nsNames := []string{sourcePod.Namespace, destinationPod.Namespace} // @etail: ok if same?
+		for _, ns := range nsNames {
+			n, err := clientset.CoreV1().Namespaces().Get(context.TODO(), ns, metav1.GetOptions{})
+			if err != nil {
+				return err
+			}
+			namespaces = append(namespaces, n)
+		}
+
+		pods := []*corev1.Pod{}
+		podNames := []types.NamespacedName{sourcePod, destinationPod}
+		for _, pod := range podNames {
+			p, err := clientset.CoreV1().Pods(pod.Namespace).Get(context.TODO(), pod.Name, metav1.GetOptions{})
+			if err != nil {
+				return err
+			}
+			pods = append(pods, p)
+		}
+
+		policies := []*netv1.NetworkPolicy{}
+		for _, ns := range nsNames {
+			npList, err := clientset.NetworkingV1().NetworkPolicies(ns).List(context.TODO(), metav1.ListOptions{})
+			if err != nil {
+				return err
+			}
+			for _, np := range npList.Items {
+				policies = append(policies, &np)
+			}
+		}
+
+		if eval.SetResources(policies, pods, namespaces) != nil {
+			return err
+		}
+
+		allowed, err := eval.CheckIfAllowed(source, destination, protocol, port)
+		if err != nil {
+			return err
+		}
+
+		// @todo: use a logger instead?
+		fmt.Printf("%v => %v over %s/%s: %t\n", source, destination, protocol, port, allowed)
+		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(evaluateCmd)
 
-	// command line flags
-	evaluateCmd.Flags().BoolVarP(&help, "help", "h", false, "display help")
-	// connection level flags
+	// connection definition
 	evaluateCmd.Flags().StringVarP(&sourcePod.Name, "source-pod", "s", sourcePod.Name, "Source pod name, required")
 	evaluateCmd.Flags().StringVarP(&sourcePod.Namespace, "source-namespace", "n", sourcePod.Namespace, "Source pod namespace")
-	evaluateCmd.Flags().StringVarP(&targetPod.Name, "destination-pod", "d", targetPod.Name, "Destination pod name")
-	evaluateCmd.Flags().StringVarP(&targetPod.Namespace, "destination-namespace", "", targetPod.Namespace, "Destination pod namespace")
+	evaluateCmd.Flags().StringVarP(&destinationPod.Name, "destination-pod", "d", destinationPod.Name, "Destination pod name")
+	evaluateCmd.Flags().StringVarP(&destinationPod.Namespace, "destination-namespace", "", destinationPod.Namespace, "Destination pod namespace")
 	evaluateCmd.Flags().StringVarP(&srcExternalIP, "source-ip", "", srcExternalIP, "Source (external) IP address")
 	evaluateCmd.Flags().StringVarP(&dstExternalIP, "destination-ip", "", dstExternalIP, "Destination (external) IP address")
 	evaluateCmd.Flags().StringVarP(&port, "destination-port", "p", port, "Destination port (name or number)")
 	evaluateCmd.Flags().StringVarP(&protocol, "protocol", "", protocol, "Protocol in use (tcp, udp, sctp)")
-	// cluster access flags
+	// cluster access
 	config := os.Getenv(clientcmd.RecommendedConfigPathEnvVar)
 	if config == "" {
 		config = clientcmd.RecommendedHomeFile
