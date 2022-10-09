667981932c2d4ccc5b3c08a753ef86118444541d
Etai Lev Ran <etail@il.ibm.com>
2022-10-09 16:49:27 +0300

    remove unused namespace and pod array

--- pkg/netpol/eval/resources.go
+++ pkg/netpol/eval/resources.go
@@ -12,8 +12,6 @@ type (
 	// PolicyEngine encapsulates the current "world view" (e.g., workloads, policies)
 	// and allows querying it for allowed or denied connections.
 	PolicyEngine struct {
-		pods         []*k8s.Pod
-		namespaces   []*k8s.Namespace
 		namspacesMap map[string]*k8s.Namespace       // map from ns name to ns object
 		podsMap      map[string]*k8s.Pod             // map from pod name to pod object
 		netpolsMap   map[string][]*k8s.NetworkPolicy // map from netpol's namespace to netpol object
@@ -23,8 +21,6 @@ type (
 // NewPolicyEngine returns a new PolicyEngine with an empty initial state
 func NewPolicyEngine() *PolicyEngine {
 	return &PolicyEngine{
-		pods:         []*k8s.Pod{},
-		namespaces:   []*k8s.Namespace{},
 		namspacesMap: make(map[string]*k8s.Namespace),
 		podsMap:      make(map[string]*k8s.Pod),
 		netpolsMap:   make(map[string][]*k8s.NetworkPolicy),
@@ -55,8 +51,6 @@ func (pe *PolicyEngine) SetResources(policies []*netv1.NetworkPolicy, pods []*co
 
 // ClearResources: deletes all current k8s resources
 func (pe *PolicyEngine) ClearResources() {
-	pe.pods = []*k8s.Pod{}
-	pe.namespaces = []*k8s.Namespace{}
 	pe.namspacesMap = map[string]*k8s.Namespace{}
 	pe.podsMap = map[string]*k8s.Pod{}
 	pe.netpolsMap = map[string][]*k8s.NetworkPolicy{}
@@ -67,7 +61,6 @@ func (pe *PolicyEngine) upsertNamespace(ns *corev1.Namespace) error {
 	if err != nil {
 		return err
 	}
-	pe.namespaces = append(pe.namespaces, nsObj)
 	pe.namspacesMap[nsObj.Name] = nsObj
 	return nil
 }
@@ -77,7 +70,6 @@ func (pe *PolicyEngine) upsertPod(pod *corev1.Pod) error {
 	if err != nil {
 		return err
 	}
-	pe.pods = append(pe.pods, podObj)
 	podStr := types.NamespacedName{Namespace: podObj.Namespace, Name: podObj.Name}
 	pe.podsMap[podStr.String()] = podObj
 	return nil
