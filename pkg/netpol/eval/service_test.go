14f21e3ecaf5ab3f5fdb56feb3703b1147d79133
shireenf-ibm <shireenf@il.ibm.com>
2023-06-18 22:07:46 +0300

    more tests

--- pkg/netpol/eval/service_test.go
+++ pkg/netpol/eval/service_test.go
@@ -1,6 +1,7 @@
 package eval
 
 import (
+	"errors"
 	"path/filepath"
 	"testing"
 
@@ -13,6 +14,7 @@ type serviceMapping struct {
 	serviceName      string
 	serviceNamespace string
 	numPods          int
+	expectedError    error
 }
 
 func TestServiceMappingToPods(t *testing.T) {
@@ -22,34 +24,56 @@ func TestServiceMappingToPods(t *testing.T) {
 			serviceName:      "demo",
 			serviceNamespace: "default",
 			numPods:          1,
+			expectedError:    nil,
 		},
 		{
 			serviceName:      "ingress-nginx-controller",
 			serviceNamespace: "ingress-nginx",
 			numPods:          1,
+			expectedError:    nil,
 		},
 		{
 			serviceName:      "ingress-nginx-controller-admission",
 			serviceNamespace: "ingress-nginx",
 			numPods:          2,
+			expectedError:    nil,
 		},
 		{
 			serviceName:      "kube-dns",
 			serviceNamespace: "kube-system",
 			numPods:          1,
+			expectedError:    nil,
+		},
+		{
+			serviceName:      "no-pods-selected",
+			serviceNamespace: "default",
+			numPods:          0,
+			expectedError:    nil,
+		},
+		{
+			serviceName:      "not-existing-svc",
+			serviceNamespace: "default",
+			numPods:          0,
+			expectedError:    errors.New("service does not exist: default/not-existing-svc"),
+		},
+		{
+			serviceName:      "not-existing-svc",
+			serviceNamespace: "not-existing-ns",
+			numPods:          0,
+			expectedError:    errors.New("service does not exist: not-existing-ns/not-existing-svc"),
 		},
 	}
 
 	path := filepath.Join(testutils.GetTestsDir(), "services", "services_with_selectors")
 	objects, processingErrs := scanner.FilesToObjectsList(path)
 	require.Len(t, processingErrs, 1) // no policies
-	require.Len(t, objects, 15)       // found 4 services and 11 pods
+	require.Len(t, objects, 16)       // found 5 services and 11 pods
 	pe, err := NewPolicyEngineWithObjects(objects)
 	require.Empty(t, err)
 
 	for _, serviceMappingItem := range serviceMappingList {
 		pods, err := pe.getServicePods(serviceMappingItem.serviceName, serviceMappingItem.serviceNamespace)
-		require.Empty(t, err)
+		require.Equal(t, serviceMappingItem.expectedError, err)
 		require.Len(t, pods, serviceMappingItem.numPods)
 	}
 }
