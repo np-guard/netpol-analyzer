f0463144ff14a37ff1f353c1ecb8f24445af9598
shireenf-ibm <82180114+shireenf-ibm@users.noreply.github.com>
2023-10-29 12:50:54 +0200

    tests improvements connlist/internal/igressanalyzer package (#251)
    * improving service_test.go
    
    * ingress analyzer tests improvements

--- pkg/netpol/internal/testutils/utils.go
+++ pkg/netpol/internal/testutils/utils.go
@@ -15,3 +15,9 @@ func GetTestsDir() string {
 	res := filepath.Join(currentDir, dirLevelUp, dirLevelUp, dirLevelUp, testsDirName)
 	return res
 }
+
+func GetTestsDirFromInternalPkg() string {
+	currentDir, _ := os.Getwd()
+	res := filepath.Join(currentDir, dirLevelUp, dirLevelUp, dirLevelUp, dirLevelUp, dirLevelUp, testsDirName)
+	return res
+}
