1e720fe50d93c979280badc452eb5839b636609e
adisos <adisos@il.ibm.com>
2023-11-05 23:02:54 +0200

    update
    Signed-off-by: adisos <adisos@il.ibm.com>

--- pkg/netpol/diff/diff_formatter_dot.go
+++ pkg/netpol/diff/diff_formatter_dot.go
@@ -207,7 +207,7 @@ func addSingleEdgeKey(src, dst, edgeLabel, edgeColor string) string {
 		edgeColor, fontColorStr, edgeColor, legendFontSize, legendArrowSize)
 }
 
-//nolint:revive // temporary work around
+//nolint:all // temporary work around
 func addEdgeKeyLines() []string { // const
 	res := []string{
 		linePrefix + listOpen + legendRankSourceLine + space + strings.Join(invisibleNodes[0:4], space) + listClose,
