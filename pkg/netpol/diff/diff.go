5aa1c5e49b458a02706c7234e39c7c84cd64978c
shireenf-ibm <shireenf@il.ibm.com>
2023-11-12 14:58:36 +0200

    fix

--- pkg/netpol/diff/diff.go
+++ pkg/netpol/diff/diff.go
@@ -11,75 +11,72 @@ import (
 	"os"
 
 	v1 "k8s.io/api/core/v1"
+	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 	"k8s.io/cli-runtime/pkg/resource"
 
 	"github.com/np-guard/netpol-analyzer/pkg/netpol/common"
 	"github.com/np-guard/netpol-analyzer/pkg/netpol/connlist"
 	"github.com/np-guard/netpol-analyzer/pkg/netpol/eval"
 	"github.com/np-guard/netpol-analyzer/pkg/netpol/logger"
+	"github.com/np-guard/netpol-analyzer/pkg/netpol/manifests/fsscanner"
+	"github.com/np-guard/netpol-analyzer/pkg/netpol/manifests/parser"
 )
 
-// more Diff Analyzer funcs:
-// DiffAnalyzerOption is the type for specifying options for DiffAnalyzer,
-// using Golang's Options Pattern (https://golang.cafe/blog/golang-functional-options-pattern.html).
-type DiffAnalyzerOption func(*DiffAnalyzer)
+// Diff Analyzer funcs:
 
-// WithLogger is a functional option which sets the logger for a DiffAnalyzer to use.
-// The provided logger must conform with the package's Logger interface.
-func WithLogger(l logger.Logger) DiffAnalyzerOption {
-	return func(c *DiffAnalyzer) {
-		c.logger = l
+// ConnDiffFromResourceInfos returns the connectivity diffs from two lists of resource.Info objects,
+// representing two versions of manifest sets to compare
+func (da *DiffAnalyzer) ConnDiffFromResourceInfos(infos1, infos2 []*resource.Info) (ConnectivityDiff, error) {
+	// connectivity analysis for first dir
+	// TODO: should add input arg dirPath to this API func? so that log msgs can specify the dir, rather then just "ref1"/"ref2"
+	conns1, workloads1, shouldStop, cDiff, errVal := da.getConnlistAnalysis(infos1, true, "")
+	if shouldStop {
+		return cDiff, errVal
 	}
+
+	// connectivity analysis for second dir
+	conns2, workloads2, shouldStop, cDiff, errVal := da.getConnlistAnalysis(infos2, false, "")
+	if shouldStop {
+		return cDiff, errVal
+	}
+
+	// the actual diff analysis
+	return da.computeDiffFromConnlistResults(conns1, conns2, workloads1, workloads2)
 }
 
-// WithOutputFormat is a functional option, allowing user to choose the output format txt/csv/md.
-func WithOutputFormat(outputFormat string) DiffAnalyzerOption {
-	return func(d *DiffAnalyzer) {
-		d.outputFormat = outputFormat
-	}
-}
+// ConnDiffFromDirPaths returns the connectivity diffs from two dir paths containing k8s resources,
+// representing two versions of manifest sets to compare
+func (da *DiffAnalyzer) ConnDiffFromDirPaths(dirPath1, dirPath2 string) (ConnectivityDiff, error) {
+	// attempt to read manifests from both dirs
+	infos1, errs1 := fsscanner.GetResourceInfosFromDirPath([]string{dirPath1}, true, da.stopOnError)
+	infos2, errs2 := fsscanner.GetResourceInfosFromDirPath([]string{dirPath2}, true, da.stopOnError)
 
-// WithStopOnError is a functional option which directs DiffAnalyzer to stop any processing after the
-// first severe error.
-func WithStopOnError() DiffAnalyzerOption {
-	return func(d *DiffAnalyzer) {
-		d.stopOnError = true
-	}
-}
+	if len(errs1) > 0 || len(errs2) > 0 {
+		if (len(infos1) == 0 && len(infos2) == 0) || da.stopOnError || !doBothInputDirsExist(dirPath1, dirPath2) {
+			err := utilerrors.NewAggregate(append(errs1, errs2...))
+			dirPath := dirPath1
+			if len(errs1) == 0 {
+				dirPath = dirPath2
+			}
+			da.logger.Errorf(err, "Error getting resourceInfos from dir paths %s/%s ", da.ref1Name, da.ref2Name)
+			da.errors = append(da.errors, parser.FailedReadingFile(dirPath, err))
+			return nil, err // return as fatal error if both infos-lists are empty, or if stopOnError is on,
+			// or if at least one input dir does not exist
+		}
 
-// WithArgNames is a functional option that sets the names to be used for the two sets of analyzed resources
-// (default is ref1,ref2) in the output reports and log messages.
-func WithArgNames(ref1Name, ref2Name string) DiffAnalyzerOption {
-	return func(d *DiffAnalyzer) {
-		d.ref1Name = ref1Name
-		d.ref2Name = ref2Name
+		// split err if it's an aggregated error to a list of separate errors
+		errReadingFile := "error reading file"
+		for _, err := range errs1 {
+			da.logger.Errorf(err, da.errPrefixSpecifyRefName(true)+errReadingFile) // print to log the error from builder
+			da.errors = append(da.errors, parser.FailedReadingFile(dirPath1, err)) // add the error from builder to accumulated errors
+		}
+		for _, err := range errs2 {
+			da.logger.Errorf(err, da.errPrefixSpecifyRefName(false)+errReadingFile) // print to log the error from builder
+			da.errors = append(da.errors, parser.FailedReadingFile(dirPath2, err))  // add the error from builder to accumulated errors
+		}
 	}
+	return da.ConnDiffFromResourceInfos(infos1, infos2)
 }
-
-// Errors returns a slice of DiffError with all warnings and errors encountered during processing.
-func (da *DiffAnalyzer) Errors() []DiffError {
-	return da.errors
-}
-
-// NewDiffAnalyzer creates a new instance of DiffAnalyzer, and applies the provided functional options.
-func NewDiffAnalyzer(options ...DiffAnalyzerOption) *DiffAnalyzer {
-	// object with default behavior options
-	da := &DiffAnalyzer{
-		logger:       logger.NewDefaultLogger(),
-		stopOnError:  false,
-		errors:       []DiffError{},
-		outputFormat: common.DefaultFormat,
-		ref1Name:     "ref1",
-		ref2Name:     "ref2",
-	}
-	for _, o := range options {
-		o(da)
-	}
-	return da
-}
-
-// ValidDiffFormats are the supported formats for output generation of the diff command
-var ValidDiffFormats = []string{common.TextFormat, common.CSVFormat, common.MDFormat, common.DOTFormat}
 
 func doBothInputDirsExist(dirPath1, dirPath2 string) bool {
 	return dirExists(dirPath1) && dirExists(dirPath2)
@@ -259,6 +256,9 @@ func getIPblocksFromConnList(conns []connlist.Peer2PeerConnection) []eval.Peer {
 	}
 	return res
 }
+
+// ValidDiffFormats are the supported formats for output generation of the diff command
+var ValidDiffFormats = []string{common.TextFormat, common.CSVFormat, common.MDFormat, common.DOTFormat}
 
 // ConnectivityDiffToString returns a string of connections diff from connectivityDiff object in the required output format
 func (da *DiffAnalyzer) ConnectivityDiffToString(connectivityDiff ConnectivityDiff) (string, error) {
