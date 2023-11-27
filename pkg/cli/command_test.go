0277197a01d4ddf38f7dec14e5e019f941118bfb
shireenf-ibm <shireenf@il.ibm.com>
2023-11-27 09:07:28 +0200

    generating dot new files (make test-update)

--- pkg/cli/command_test.go
+++ pkg/cli/command_test.go
@@ -315,10 +315,12 @@ func TestCommands(t *testing.T) {
 				"dot",
 			},
 			expectedOutput: "digraph {\n" +
-				"\t\"default/checkoutservice[Deployment]\" [label=\"default/checkoutservice[Deployment]\" color=\"blue\" fontcolor=\"blue\"]\n" +
-				"\t\"default/emailservice[Deployment]\" [label=\"default/emailservice[Deployment]\" color=\"blue\" fontcolor=\"blue\"]\n" +
-				"\t\"default/checkoutservice[Deployment]\" -> \"default/emailservice[Deployment]\"" +
-				" [label=\"TCP 8080\" color=\"gold2\" fontcolor=\"darkgreen\"]\n" +
+				"\tsubgraph cluster_default {\n" +
+				"\t\t\"checkoutservice\" [label=\"checkoutservice\" color=\"blue\" fontcolor=\"blue\"]\n" +
+				"\t\t\"emailservice\" [label=\"emailservice\" color=\"blue\" fontcolor=\"blue\"]\n" +
+				"\t\tlabel=\"default\"\n" +
+				"\t}\n" +
+				"\t\"checkoutservice\" -> \"emailservice\" [label=\"TCP 8080\" color=\"gold2\" fontcolor=\"darkgreen\"]\n" +
 				"}",
 			exact: true,
 			isErr: false,
