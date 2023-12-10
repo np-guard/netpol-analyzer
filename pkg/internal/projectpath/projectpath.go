732f1ca8319d9dcd35ddaa19b2e538fd9e1405a8
shireenf-ibm <82180114+shireenf-ibm@users.noreply.github.com>
2023-12-10 14:07:33 +0200

    Improve test dir paths handling  (#285)
    * new way to get tests/test_outputs dirs
    
    * eliminating duplicates

---
+++ pkg/internal/projectpath/projectpath.go
@@ -0,0 +1,15 @@
+package projectpath
+
+import (
+	"path/filepath"
+	"runtime"
+)
+
+const dirLevelUp = ".."
+
+var (
+	_, b, _, _ = runtime.Caller(0)
+
+	// Root folder of this project
+	Root = filepath.Join(filepath.Dir(b), dirLevelUp, dirLevelUp, dirLevelUp)
+)
