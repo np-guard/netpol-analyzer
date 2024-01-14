e226a20f718c1bd90ee00c9c159b4f34272000dd
shireenf-ibm <shireenf@il.ibm.com>
2024-01-14 15:09:22 +0200

    interface doc update

--- pkg/netpol/connlist/exposed_pods.go
+++ pkg/netpol/connlist/exposed_pods.go
@@ -6,21 +6,22 @@ import "github.com/np-guard/netpol-analyzer/pkg/netpol/internal/common"
 type ExposedPeer interface {
 	// ExposedPeer is a peer for which the analysis found some potential exposure info
 	ExposedPeer() Peer
-	// IngressExposure list of the potential Ingress connections to the ExposedPeer
+	// IngressExposure is a list of the potential Ingress connections to the ExposedPeer
 	IngressExposure() []XgressExposureData
-	// EgressExposure list of the potential Egress connections from the ExposedPeer
+	// EgressExposure is a list of the potential Egress connections from the ExposedPeer
 	EgressExposure() []XgressExposureData
 }
 
-// XgressExposureData data of potential connectivity for an existing peer to/from a representative peer
+// XgressExposureData contains data of potential connectivity for an existing peer to/from a representative peer
 type XgressExposureData interface {
-	// IsProtectedByNetpols indicates if the exposed peer is protected by any netpol on Ingress/Egress 
+	// IsProtectedByNetpols indicates if the exposed peer is protected by any netpol on Ingress/Egress
+	// if a peer is not protected by xgress netpols, it will be exposed to entire cluster with all allowed connections
 	IsProtectedByNetpols() bool
-	// IsExposedToEntireCluster is the peer exposed to all namespaces in the cluster for the relevant direction
+	// IsExposedToEntireCluster indicates if the peer is exposed to all namespaces in the cluster for the relevant direction
 	IsExposedToEntireCluster() bool
-	// NamespaceLabels the potential namespaces which the peer is exposed to by their labels
+	// NamespaceLabels are matchLabels of potential namespaces which the peer might be exposed to
 	NamespaceLabels() map[string]string
-	// PodLabels the potential pods which the peer might be exposed to by labels
+	// PodLabels are matchLabels of potential pods which the peer might be exposed to
 	PodLabels() map[string]string
 	// PotentialConnectivity the potential connectivity of the exposure
 	PotentialConnectivity() common.AllowedConnectivity
