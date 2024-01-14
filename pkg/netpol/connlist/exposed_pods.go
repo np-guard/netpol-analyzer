19ce1b26dc5f5a4bc8ad19052d6ff5f66e76605b
shireenf-ibm <shireenf@il.ibm.com>
2024-01-14 11:31:32 +0200

    wip- some updates to the interfaces (only)

--- pkg/netpol/connlist/exposed_pods.go
+++ pkg/netpol/connlist/exposed_pods.go
@@ -5,13 +5,23 @@ import "github.com/np-guard/netpol-analyzer/pkg/netpol/internal/common"
 // ExposedPeer captures potential ingress and egress connections data for an exposed Peer
 type ExposedPeer interface {
 	// ExposedPeer is a peer for which the analysis found some potential exposure info
-	ExposedPeer() Peer 
+	ExposedPeer() Peer
+	// IngressExposure list of the potential Ingress connections to the ExposedPeer
 	IngressExposure() []XgressExposureData
+	// EgressExposure list of the potential Egress connections from the ExposedPeer
 	EgressExposure() []XgressExposureData
 }
 
 // XgressExposureData data of potential connectivity for an existing peer to/from a representative peer
 type XgressExposureData interface {
-	RepresentativePeer() Peer
+	// IsProtectedByNetpols Is the exposed peer protected by any xgress netpol
+	IsProtectedByNetpols() bool
+	// IsExposedToEntireCluster is the peer exposed to all namespaces in the cluster for the relevant direction
+	IsExposedToEntireCluster() bool
+	// NamespaceLabels the potential namespaces which the peer is exposed to by their labels
+	NamespaceLabels() map[string]string
+	// PodLabels the potential pods which the peer might be exposed to by labels
+	PodLabels() map[string]string
+	// PotentialConnectivity the potential connectivity of the exposure
 	PotentialConnectivity() common.AllowedConnectivity
 }
