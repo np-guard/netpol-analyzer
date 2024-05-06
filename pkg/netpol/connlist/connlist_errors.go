d8e574488bd9155616f3c9a7c82e0b3740a41077
adisos <adisos@il.ibm.com>
2024-05-06 12:43:32 +0300

    lint fix: the type name connlistAnalyzerWarning should conform to the xxxError format
    Signed-off-by: adisos <adisos@il.ibm.com>

--- pkg/netpol/connlist/connlist_errors.go
+++ pkg/netpol/connlist/connlist_errors.go
@@ -29,7 +29,7 @@ type resourceEvaluationError struct {
 	origErr error
 }
 
-type connlistAnalyzerWarning struct {
+type connlistAnalyzerWarnError struct {
 	origErr error
 }
 
@@ -41,7 +41,7 @@ func (e *resourceEvaluationError) Error() string {
 	return e.origErr.Error()
 }
 
-func (e *connlistAnalyzerWarning) Error() string {
+func (e *connlistAnalyzerWarnError) Error() string {
 	return e.origErr.Error()
 }
 
@@ -76,5 +76,5 @@ func newResourceEvaluationError(err error) *connlistGeneratingError {
 }
 
 func newConnlistAnalyzerWarning(err error) *connlistGeneratingError {
-	return &connlistGeneratingError{err: &connlistAnalyzerWarning{err}, fatal: false, severe: false}
+	return &connlistGeneratingError{err: &connlistAnalyzerWarnError{err}, fatal: false, severe: false}
 }
