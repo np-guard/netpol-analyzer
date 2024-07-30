67361d7ffe4b9c164ca03133c81327c5b1083fb5
shireenf-ibm <82180114+shireenf-ibm@users.noreply.github.com>
2024-07-30 20:47:37 +0300

    Update pkg/netpol/eval/internal/k8s/netpol.go
    Co-authored-by: Adi Sosnovich <82078442+adisos@users.noreply.github.com>

--- pkg/netpol/eval/internal/k8s/netpol.go
+++ pkg/netpol/eval/internal/k8s/netpol.go
@@ -481,7 +481,7 @@ type SingleRuleSelectors struct {
 }
 
 // ScanPolicyRulesAndUpdateExposedWideConns scans policy rules and :
-// - updates policy's exposed wide connections from/to external resources or/and from/to all namespaces in the cluster on
+// - updates policy's exposed cluster-wide connections from/to external resources or/and from/to all namespaces in the cluster on
 // ingress and egress directions
 // - returns list of labels.selectors from rules which have non-empty selectors, for which the representative peers should be generated
 func (np *NetworkPolicy) ScanPolicyRulesAndUpdateExposedWideConns() (rulesSelectors []SingleRuleSelectors, err error) {
