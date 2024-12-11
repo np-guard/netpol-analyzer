fbace5b6ab4eeb3f908dac2a3534302f81477486
shireenf-ibm <shireenf@il.ibm.com>
2024-12-11 13:47:00 +0200

    avoid dup.

--- pkg/netpol/eval/resources.go
+++ pkg/netpol/eval/resources.go
@@ -676,26 +676,15 @@ func (pe *PolicyEngine) createPodOwnersMap() (map[string]Peer, error) {
 // get peers in level of workloads (pod owners) of type WorkloadPeer, and ip-blocks
 // Deprecated - instead using funcs GetWorkloadPeersList and GetIPBlockPeersLists
 func (pe *PolicyEngine) GetPeersList() ([]Peer, error) {
-	podOwnersMap, err := pe.createPodOwnersMap()
+	_, _, res, err := pe.GetIPBlockPeersLists()
 	if err != nil {
 		return nil, err
 	}
-	_, _, ipBlocks, err := pe.getDisjointIPBlocks()
+	workloads, err := pe.GetWorkloadPeersList()
 	if err != nil {
 		return nil, err
 	}
-
-	// add ip-blocks to peers list
-	res := make([]Peer, len(ipBlocks)+len(podOwnersMap))
-	for i := range ipBlocks {
-		res[i] = &k8s.IPBlockPeer{IPBlock: ipBlocks[i]}
-	}
-	index := len(ipBlocks)
-	// add workload peer objects to peers list
-	for _, workloadPeer := range podOwnersMap {
-		res[index] = workloadPeer
-		index++
-	}
+	res = append(res, workloads...)
 	return res, nil
 }
 
